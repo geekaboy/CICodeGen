6b0c17ce43f55afd36f829210a969ff11d6e1751
@geekaboy <geekaboys@gmail.com>
2019-10-02 11:14:25 +0700

    update info

--- appsystem/views/home/main_view.php
+++ appsystem/views/home/main_view.php
@@ -14,22 +14,40 @@
                                 <img src="<?php echo base_url('assets/images/sys_logo.png'); ?>" class="img" width="350" />
 
                             </div>
-                            <div class="text-center">
+                            <div class="text-center mt-3">
+                                <h4><u><i class="fa fa-info-circle" aria-hidden="true"></i> Info</u> </h4>
+                                Version => 0.1 Bata<br>
+                                Repo => <a href="https://gitlab.com/geekaboy/cicodegen" target="_blank">https://gitlab.com/geekaboy/cicodegen</a><br>
                                 Developer => SS2SEK<br>
                                 Contact => suchartsek.p@gmail.com<br>
-                                Repo => https://gitlab.com/geekaboy/cicodegen<br>
-                                Version => 0.1 Bata<br>
+
+
                                 Update lastest => <?php echo date('Y-m-d'); ?><br>
                                 License => GPL 3
                             </div>
-                            <br>
-                            <div class="text-center">
-                                <h4><u>Dev. on:</u> </h4>
+                            <div class="text-center mt-3">
+                                <h4><u><i class="fa fa-code" aria-hidden="true"></i> Dev. on:</u> </h4>
                                 Database => PostgreSQL 10.10, MySQL 5.7.26 <br>
                                 OS => Ubuntu 18.04.1 LTS<br>
                                 HTTP Server => Apache2.2.34 <br>
                                 PHP => 7.2<br>
-                                Framework => codeigniter 3<br>
+                                Framework => Codeigniter 3<br>
+                            </div>
+                            <div class="text-center mt-3">
+                                <h4><u><i class="fa fa-coffee" aria-hidden="true"></i> Buy me a coffee :)</u> </h4>
+                                <div class="row justify-content-center">
+                                  <div class="col-md-2 text-center">
+                                    <img src="<?php echo base_url('assets/images/promptpay.png'); ?>" width="150" alt="">
+                                  </div>
+                                  <div class="col-md-2 text-center">
+                                      <a href="https://paypal.me/ss2sek" target="_blank">
+                                          <img src="<?php echo base_url('assets/images/paypal-me.png'); ?>" width="150" alt="">
+                                      </a><br>
+                                      <a href="https://paypal.me/ss2sek" target="_blank">https://paypal.me/ss2sek</a>
+                                  </div>
+                                </div>
+
+
                             </div>
                         </div>
 
